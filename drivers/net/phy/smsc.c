fcf84605a23a095ec35ea158547172bc244bc1fb
yuanye0814 <yuanye0814@hotmail.com>
2025-10-10 00:59:23 +0800

    * config fec for LAN8720

--- drivers/net/phy/smsc.c
+++ drivers/net/phy/smsc.c
@@ -67,21 +67,23 @@ static int smsc_phy_reset(struct phy_device *phydev)
 	 * in all capable mode before using it.
 	 */
 	if ((rc & MII_LAN83C185_MODE_MASK) == MII_LAN83C185_MODE_POWERDOWN) {
-		int timeout = 50000;
-
+		
 		/* set "all capable" mode and reset the phy */
 		rc |= MII_LAN83C185_MODE_ALL;
 		phy_write(phydev, MII_LAN83C185_SPECIAL_MODES, rc);
-		phy_write(phydev, MII_BMCR, BMCR_RESET);
-
-		/* wait end of reset (max 500 ms) */
-		do {
-			udelay(10);
-			if (timeout-- == 0)
-				return -1;
-			rc = phy_read(phydev, MII_BMCR);
-		} while (rc & BMCR_RESET);
 	}
+	
+	phy_write(phydev, MII_BMCR, BMCR_RESET);
+	
+	int timeout = 50000;
+	/* wait end of reset (max 500 ms) */
+	do {
+		udelay(10);
+		if (timeout-- == 0)
+			return -1;
+		rc = phy_read(phydev, MII_BMCR);
+	} while (rc & BMCR_RESET);
+	
 	return 0;
 }
 
