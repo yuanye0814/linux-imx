2037d3bc160148a28a189fce5d9f3a5afda0acb6
yuanye0814 <yuanye0814@hotmail.com>
2026-01-16 17:55:49 +0800

    + ME3630 driver

--- drivers/usb/serial/option.c
+++ drivers/usb/serial/option.c
@@ -624,6 +624,10 @@ static const struct option_blacklist_info sierra_mc73xx_blacklist = {
 };
 
 static const struct usb_device_id option_ids[] = {
+	{ USB_DEVICE(0x19d2, 0x0117) }, /* ME3630*/
+	{ USB_DEVICE(0x19d2, 0x0199) }, /* ME3630*/
+	{ USB_DEVICE(0x19d2, 0x1476) }, /* ME3630*/
+
 	{ USB_DEVICE(OPTION_VENDOR_ID, OPTION_PRODUCT_COLT) },
 	{ USB_DEVICE(OPTION_VENDOR_ID, OPTION_PRODUCT_RICOLA) },
 	{ USB_DEVICE(OPTION_VENDOR_ID, OPTION_PRODUCT_RICOLA_LIGHT) },
@@ -1880,6 +1884,27 @@ static int option_probe(struct usb_serial *serial,
 	if (dev_desc->idVendor == cpu_to_le16(SAMSUNG_VENDOR_ID) &&
 	    dev_desc->idProduct == cpu_to_le16(SAMSUNG_PRODUCT_GT_B3730) &&
 	    iface_desc->bInterfaceClass != USB_CLASS_CDC_DATA)
+		return -ENODEV;
+
+	// ME3630
+	if (serial->dev->descriptor.idVendor == 0x19d2 &&
+		serial->dev->descriptor.idProduct == 0x1476 &&
+		serial->interface->cur_altsetting->desc. bInterfaceNumber == 3)
+		return -ENODEV;
+
+	if (serial->dev->descriptor.idVendor == 0x19d2 &&
+		serial->dev->descriptor.idProduct == 0x1476 &&
+		serial->interface->cur_altsetting->desc. bInterfaceNumber == 4)
+		return -ENODEV;
+
+	if (serial->dev->descriptor.idVendor == 0x19d2 &&
+		serial->dev->descriptor.idProduct == 0x1509 &&
+		serial->interface->cur_altsetting->desc. bInterfaceNumber == 4)
+		return -ENODEV;
+
+	if (serial->dev->descriptor.idVendor == 0x19d2 &&
+		serial->dev->descriptor.idProduct == 0x1509 &&
+		serial->interface->cur_altsetting->desc. bInterfaceNumber == 5)
 		return -ENODEV;
 
 	/* Store the blacklist info so we can use it during attach. */
